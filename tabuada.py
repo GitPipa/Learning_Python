994a738e390878807e727cff094fc0e55602f3b9
GitPipa <122698000+GitPipa@users.noreply.github.com>
2023-01-16 11:44:06 +0000

    Added tabuada
    first tabuada, challenged by GB

---
+++ tabuada.py
@@ -0,0 +1,21 @@
+def tabuada():
+    for i in range(1000):
+        x = input("Eu digo-te a tabuada. Queres de que número? ")
+        y = int(x)
+        print()
+        print("0 x " + x + " =", 0 * y)
+        print("1 x " + x + " =", 1 * y)
+        print("2 x " + x + " =", 2 * y)
+        print("3 x " + x + " =", 3 * y)
+        print("4 x " + x + " =", 4 * y)
+        print("5 x " + x + " =", 5 * y)
+        print("6 x " + x + " =", 6 * y)
+        print("7 x " + x + " =", 7 * y)
+        print("8 x " + x + " =", 8 * y)
+        print("9 x " + x + " =", 9 * y)
+        print("10 x " + x + " =", 10 * y)
+        print()
+
+
+tabuada()
+
