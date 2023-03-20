ec2a6461266d6236ff236f280bddd848a70b3dc6
GitPipa <122698000+GitPipa@users.noreply.github.com>
2023-03-20 14:39:06 +0000

    Update tabuada.py
    Finally added loop for the sequence, much efficient now. Added int instead of eval.

--- tabuada.py
+++ tabuada.py
@@ -1,20 +1,11 @@
 def tabuada():
     for i in range(1000):
-        x = eval(input("Eu digo-te a tabuada. Queres de que número? "))
-
+        x = int(input("Eu digo-te a tabuada. Queres de que número? "))
+        y=-1
         print()
-        print("0 x", x, "=", 0 * x)
-        print("1 x", x, "=", 1 * x)
-        print("2 x", x, "=", 2 * x)
-        print("3 x", x, "=", 3 * x)
-        print("4 x", x, "=", 4 * x)
-        print("5 x", x, "=", 5 * x)
-        print("6 x", x, "=", 6 * x)
-        print("7 x", x, "=", 7 * x)
-        print("8 x", x, "=", 8 * x)
-        print("9 x", x, "=", 9 * x)
-        print("10 x", x, "=", 10 * x)
-        print()
+        for i in range(11):
+            y=y+1
+            print(y,"x", x, "=", y * x)
 
 tabuada()
 
